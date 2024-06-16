c9cd73ccf4cbd31ff333e22e8680f577bb3808ca
ROBERTO ANTONIO ALFERES GOMEZ <roberto3652017@gmail.com>
2024-06-15 20:25:19 -0600

    deshabilitando api de obtener todos los estudiantes

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,7 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/estudiantes/all', [EstudianteController::class, 'index']);
+//Route::get('/estudiantes/all', [EstudianteController::class, 'index']);
 Route::post('/estudiantes/register', [EstudianteController::class, 'register']);
 Route::post('/questions/get', [QuestionsController::class, 'get']);
 Route::post('/answers/create', [AnswersController::class, 'create']);
